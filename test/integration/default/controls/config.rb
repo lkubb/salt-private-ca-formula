52ec96868a6d2066dccdef889dadb01c480fb55e
jeanluc <lkubb@protonmail.com>
2023-12-26 15:27:21 +0100

    chore: Update template

--- test/integration/default/controls/config.rb
+++ test/integration/default/controls/config.rb
@@ -24,10 +24,10 @@ control 'pca.config.file' do
       # rubocop:disable Lint/RedundantCopDisableDirective
       # rubocop:disable Layout/LineLength
       should include(
-        '"tofs": {"files_switch": ["any/path/can/be/used/here", "id", '\
-        '"roles", "osfinger", "os", "os_family"], "source_files": '\
-        '{"pca-config-file-file-managed": ["example.tmpl.jinja"], '\
-        '"pca--config-file-file-managed": '\
+        '"tofs": {"files_switch": ["any/path/can/be/used/here", "id", ' \
+        '"roles", "osfinger", "os", "os_family"], "source_files": ' \
+        '{"pca-config-file-file-managed": ["example.tmpl.jinja"], ' \
+        '"pca--config-file-file-managed": ' \
         '["-example.tmpl.jinja"]}'
       )
       # rubocop:enable Layout/LineLength
